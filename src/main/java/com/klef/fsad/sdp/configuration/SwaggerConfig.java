6c295926b46f58d898ae3292ac6f323611a147ef
dineshchanduaragala <2400030518@kluniversity.in>
2026-03-28 00:10:32 +0530

    added config file

--- src/main/java/com/klef/fsad/sdp/configuration/SwaggerConfig.java
+++ src/main/java/com/klef/fsad/sdp/configuration/SwaggerConfig.java
@@ -1,5 +1,21 @@
 package com.klef.fsad.sdp.configuration;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig 
+{
+    @Bean
+    public OpenAPI customOpenAPI() 
+    {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tourism Homestay API")
+                        .version("1.0")
+                        .description("Backend APIs for Tourism Homestay Platform"));
+    }
 }
